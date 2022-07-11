853d7697f94d3000a6fc9df0f4366954b0847068
mariadc21! <mariferdc2002@gmail.com>
2022-07-11 13:11:25 -0400

    finished challenges

--- challeng.js
+++ challeng.js
@@ -12,6 +12,8 @@ function min(){
 }
 min();
 
+//Easy
+
 let students =  [
     {'first':'Bob',
     'last': 'Smith' ,
@@ -23,6 +25,8 @@ let students =  [
     'last' : 'William',
     'age': 16}]
 alert(`Hello my name is ${students[1]['first']} ${students[1]['last']} and I'm ${students[1]['age']}`)
+
+//medium
 
 let months =['Null','January','February','March','April','May',
 'June','July','August','September','October','November','December']
@@ -37,3 +41,25 @@ else if (mon > 12){
 else{
     console.log(`${months[mon]}`);
 }
+
+//Hard
+
+let tom =[{"height":9},{"mass": 8}]
+let jerry =[{"height":10},{"mass": 45}]
+let BMIs=[]
+
+function BMIfun(mass,height){
+  let  calculation = mass/(height*height)
+    BMIs.push(calculation)
+    console.log(BMIs)
+    alert (BMIs)
+}
+
+BMIfun(tom[1]["mass"],tom[0]["height"])
+BMIfun(jerry[1]["mass"],jerry[0]["height"])
+
+console.log(BMIs[0])
+console.log(BMIs[1])
+let answer=(BMIs[0]) > (BMIs[1])
+console.log(`Is Tom's BMI higher than Jerry's? ${answer}`)
+
